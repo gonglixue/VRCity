8c17de9ffae58b53f867c50c4cdb6327d0bb4d63
gonglixue <724351282@qq.com>
2017-03-05 19:10:06 +0800

    basic

---
+++ Assets/Scripts/BuildingGeoList.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingGeoList : MonoBehaviour {
+    public static ArrayList buildingGeoList = new ArrayList();
+    
+	// Use this for initialization
+	void Start () {
+        // longitude, latitude
+        buildingGeoList.Add(new Vector2(13.3968027f, 52.5414223f));
+        buildingGeoList.Add(new Vector2())
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    struct buidlingInfo
+    {
+        double latitude;
+        double longitude;
+        double altitude;
+        double hading;
+        string name;
+        string modelHref;
+    }
+}
---
+++ Assets/Scripts/util/XMLParser.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class XMLParser {
+    public static string name = "Allfun_变量";// 静态变量 供外部访问
+}
